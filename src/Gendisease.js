9a5875f0179d8352d2f3a7c6b5672c9a863131d1
SmitMehta50 <69676418+SmitMehta50@users.noreply.github.com>
2022-04-24 17:17:10 +0530

    added hospital component as teest.js

--- src/Gendisease.js
+++ src/Gendisease.js
@@ -11,8 +11,8 @@ function Gendisease() {
       const [selectedValue, setSelectedValue] = useState([""]);
       const [message, setMessage] = useState("");
       const [discription, setDiscription] = useState("");
-      const [treatment, setTreatment] = useState("");
-      const [rate, setRate] = useState("")
+      const [treatments, setTreatments] = useState([]);
+      const [rates, setRates] = useState([])
     
       // handle onChange event of the dropdown
       const handleChange = (e) => {
@@ -30,14 +30,21 @@ function Gendisease() {
             // if (response.status === 200){
             setMessage(response.data[0].prediction)
             setDiscription(response.data[2].discription)
-            setTreatment(response.data[3].treatment)
-            setRate(response.data[1]["rate"])
+            setTreatments(response.data[3].treatment)
+            setRates(response.data[1]["rate"])
+            
         
             // }else{setMessage("Error Occured")}
             // setMessage(response)
        //Perform action based on response
         }).catch((error)=>console.log(error));
       };
+      const treat = treatments.map((treatment)=>{
+        return <li key={treatment.toString()}>
+          {treatment}
+          </li>;
+        });
+
   return (
     <div>
         <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>Gendisease</h1>
@@ -57,12 +64,18 @@ function Gendisease() {
             <div><b>Selected Value: </b> {JSON.stringify(selectedValue, null, 2)}</div>
         </div>}
         <button className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded' type="submit">Submit</button>
-
-        <div className="message"><h3>Status :</h3> {message }</div>
-        <div className="prob"><h3>Description :</h3> {discription}</div>
-        <div><h3>Treatment :</h3>{JSON.stringify(treatment)}</div>
-        <div><h3>Rate :</h3>{JSON.stringify(rate)}</div>
       </form>
+      <div className="message"><h3>Status :</h3> {message }</div>
+        <div className="prob"><h3>Description :</h3> {discription}</div>
+        {/* <div><h3>Treatment :</h3>
+          {treatment.map(treat=>(
+            {treat}
+          ))}
+        </div> */}
+        
+        
+        <div><h3>Treatment :</h3>{treat}</div>
+        <div><h3>Rate :</h3>{JSON.stringify(rates)}</div>
       
     </div>
   )
--- src/test.js
+++ src/test.js
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Test() {
-    const[latitude, setLatitude] = useState('');
-    const[longitude,setLongitude] = useState('');
     const[hosdata, setHosdata] = useState([])
-    const[pin,setPin] = useState('');
+    const[latitude, setLatitude] = useState("");
+    const[longitude,setLongitude] = useState("");
+    const[pin,setPin] = useState("");
+    const[hospital1, setHospital1] = useState([])
+    const[hospital2, setHospital2] = useState([])
+    const[hospital3, setHospital3] = useState([])
+    const[hospital4, setHospital4] = useState([])
+    const[hospital5, setHospital5] = useState([])
+    const[hospital6, setHospital6] = useState([])
+    // const[hospital7, setHospital7] = useState([])
+    // const[hospital8, setHospital8] = useState([])
 
     useEffect(() => {
         if (!("geolocation" in navigator)) {
@@ -25,44 +33,207 @@ function Test() {
             var mylocation={
                 lat:latitude,
                 lng:longitude,
-                pincode: pin
+                pincode: ""
             }
 
             axios.post('https://kidneydisease-api.herokuapp.com/hospitaldata', mylocation)
             .then(function(response){
-                console.log(response.data);
-                setHosdata(response.data);
+                console.log(response.status);
+                // setHosdata(JSON.stringify(response.data))
+                setHosdata(response.status);
+                setHospital1(response.data[0])
+                setHospital2(response.data[1])
+                setHospital3(response.data[2])
+                setHospital4(response.data[3])
+                setHospital5(response.data[4])
+                setHospital6(response.data[5])
+                // setHospital7(response.data[6])
+                // setHospital8(response.data[7])
+                // console.log(hosdata);
             }).catch((error)=>console.log(error));
 
         }
-    },[latitude, longitude, pin]);
+    },[latitude, longitude]);
+    // console.log(hosdata);
 
-    let handleSubmit = async (e) => {
-        setLatitude("");
-        setLongitude("");
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        
 
         var myParams={
-            lat:latitude,
-            lng:longitude,
+            lat:"",
+            lng:"",
             pincode: pin
         }
+        console.log(myParams)
         
         await axios.post('https://kidneydisease-api.herokuapp.com/hospitaldata', myParams)
         .then(function(response){
-            console.log(response.data);
-            setHosdata(response.data);
+            // console.log(response.data);
+            setHosdata(response.status);
+            console.log(hosdata);
+            setHospital1(response.data[0])
+            setHospital2(response.data[1])
+            setHospital3(response.data[2])
+            setHospital4(response.data[3])
+            setHospital5(response.data[4])
+            setHospital6(response.data[5])
+            // setHospital7(response.data[6])
+            // setHospital8(response.data[7])
         }).catch((error)=>console.log(error));
     }
 
-  
+    const hospitaldata = () => {
+        if(hosdata===200){
+            return(
+                <div>
+                    <div className='flex flex-wrap justify-around ml-5 mr-5 mt-5 mb-5 '>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital1.City}, {hospital1.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital1.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital1.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital1.Name},+${hospital1.Address},+${hospital1.City},+${hospital1.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                        Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital1.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital2.City}, {hospital2.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital2.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital2.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital2.Name},+${hospital2.Address},+${hospital2.City},+${hospital2.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                        Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital2.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital3.City}, {hospital3.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital3.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital3.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital3.Name},+${hospital3.Address},+$6hospital3.City},+${hospital3.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                        Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital3.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital4.City}, {hospital4.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital4.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital4.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital4.Name},+${hospital4.Address},+${hospital4.City},+${hospital4.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                        Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital4.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital5.City}, {hospital5.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital5.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital5.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital5.Name},+${hospital5.Address},+${hospital5.City},+${hospital5.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                        Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital5.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center mt-4'>
+                            <div className='block rounded-lg shadow-lg bg-white max-w-sm text-center'>
+                                <div className='py-3 px-6 border-b border-gray-300'>
+                                    {hospital6.City}, {hospital6.zipcode}
+                                </div>
+                                <div  className='p-6'>
+                                    <h5 className='text-gray-900 text-xl font-medium mb-2'>{hospital6.Name}</h5>
+                                    <p className='text-gray-700 text-base mb-4'> 
+                                        {hospital6.Address}
+                                    </p>
+                                    <a href={`http://maps.google.com/maps?q=${hospital6.Name},+${hospital6.Address},+${hospital6.City},+${hospital6.zipcode}`}>
+                                        <button type='button' className='inline-block px-6 py-2.5 text-indigo-700 bg-indigo-100 hover:bg-indigo-200 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
+                                            Go To Maps
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className='py-3 px-6 border-t border-gray-300 text-gray-600'>
+                                    {hospital6.Distance}
+                                </div>
+                            </div>
+                        </div>
+
+
+</div>
+                </div>
+            );
+        }
+    }
+
 
   return (
+      
     <div>
         <div>
             <form className='w-full' onSubmit={handleSubmit}>
-                <div className='flex flex-wrap mx-3 mb-6'>
+                <div className='flex flex-wrap justify-center mx-3 mb-6'>
                     <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
                         <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
                             Pincode
@@ -76,7 +247,7 @@ function Test() {
                         <p className='text-gray-500 text-xs italic'>Fill out this field</p>
                     </div>
                 </div>
-                <div className='md:flex lg:flex md:items-center lg:items-center'>
+                <div className='md:flex justify-center lg:flex md:items-center lg:items-center'>
                     <div className='md:w-1/3'>
                         <button className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded' type="submit">
                             Submit
@@ -84,15 +255,8 @@ function Test() {
                     </div>
                 </div>
             </form>
-            <div>
-                {/* {hosdata.map(hos => (
-                    <p 
-                    // onClick={()=> handleClick(hos)}
-                    >
-                        {hos.Name}
-                    </p>
-                ))} */}
-            </div>
+            {hospitaldata()}
+            
         </div>
     </div>
   )
